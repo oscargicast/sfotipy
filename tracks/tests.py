e07073b8fd95e6ea88e4ecb57b6786d1cb26eebc
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-06-06 00:42:36 -0500

    add basic test for Track model

--- tracks/tests.py
+++ tracks/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Track
+from artists.models import Artist
+from albums.models import Album
 
-# Create your tests here.
+
+class TrackTestCase(TestCase):
+    def setUp(self):
+        self.artist = Artist.objects.create(
+            first_name="beyonce",
+            last_name="giselle knowles",
+            biography="American singer and actress...",
+        )
+        self.album = Album.objects.create(
+            title="Love Songs",
+            artist=self.artist,
+        )
+        self.track = Track.objects.create(
+            title="xo",
+            order=15,
+            album=self.album,
+            artist=self.artist,
+        )
+
+    def test_track(self):
+        self.assertEqual(self.track.title, "xo")
+        self.assertEqual(self.track.album.title, "Love Songs")
