d908cd7fd589f329a39acabf101a388cdbc19138
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-06-06 17:41:10 -0500

    return json in track view

--- tracks/views.py
+++ tracks/views.py
@@ -1,7 +1,20 @@
+import json
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Track
 
 
 def track_view(request, title):
     track = get_object_or_404(Track, title=title)
-    return render(request, 'track.html', {'track': track})
+    data = {
+        'title': track.title,
+        'order': track.order,
+        'album': track.album.title,
+        'artist': {
+            'name': track.artist.first_name,
+            'bio': track.artist.biography,
+        }
+    }
+    json_data = json.dumps(data)
+
+    return HttpResponse(json_data, content_type='application/json')
