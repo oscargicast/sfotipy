841dea7a4890fb341dd6f152913d3a23183e5fef
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-06-07 02:08:39 -0500

    validate form in signup view

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from .forms import UserCreationEmailForm
+from .forms import UserCreationEmailForm, EmailAuthenticationForm
 
 
 def signup(request):
-    form = UserCreationEmailForm()
+    form = UserCreationEmailForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+
     return render(request, 'signup.html', {'form': form})
