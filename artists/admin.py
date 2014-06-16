2e44470c1e4b53944dd24ce711090075a7853795
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-06-16 00:48:10 -0500

    create artist admin using inlines and filter horizontal and vertical, both useful relations

--- artists/admin.py
+++ artists/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Artist
+from tracks.models import Track
+from albums.models import Album
 
 
-admin.site.register(Artist)
+class TrackInLine(admin.StackedInline):
+    model = Track
+    extra = 1
+
+
+class AlbumInLine(admin.StackedInline):
+    model = Album
+    extra = 1
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    search_fields = ('first_name', 'last_name', )
+    inlines = [AlbumInLine, TrackInLine, ]
+    filter_horizontal = ('favorite_songs', )
+    # filter_vertical = ('favorite_songs', )
+
+admin.site.register(Artist, ArtistAdmin)
