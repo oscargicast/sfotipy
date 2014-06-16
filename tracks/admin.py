3c0adcb6238cbead6cb26a682b19e6a4830e14fb
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-06-15 21:36:29 -0500

    create admin for tracks

--- tracks/admin.py
+++ tracks/admin.py
@@ -1,5 +1,31 @@
+from actions import export_as_excel
 from django.contrib import admin
 from .models import Track
 
 
-admin.site.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = (
+        'artist',
+        'title',
+        'order',
+        'album',
+        'player',
+        'is_top_ten',
+    )
+    list_filter = ('artist', 'album', )
+    search_fields = (
+        'title',
+        'artist__first_name',
+        'artist__last_name',
+        'album__title',
+    )
+    list_editable = ('title', 'album', )
+    actions = (export_as_excel, )
+    raw_id_fields = ('artist', )
+
+    def is_top_ten(self, obj):
+        return obj.order <= 10
+
+    is_top_ten.boolean = True
+
+admin.site.register(Track, TrackAdmin)
--- tracks/models.py
+++ tracks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import format_html
 
 
 class Track(models.Model):
@@ -7,6 +8,17 @@ class Track(models.Model):
     track_file = models.FileField(upload_to='tracks')
     album = models.ForeignKey('albums.Album')
     artist = models.ForeignKey('artists.Artist')
+
+    def player(self):
+        return """
+        <audio controls>
+            <source src="%s" type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio>
+        """ % self.track_file.url
+
+    player.allow_tags = True
+    player.admin_order_field = 'track_file'
 
     def __unicode__(self):
         return self.title
