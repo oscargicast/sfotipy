5b83ce31d5886be4d09a1bf16b3fdf38f8b23789
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-06-16 00:46:33 -0500

    create many to many relationship between artist and track model

--- artists/models.py
+++ artists/models.py
@@ -5,6 +5,11 @@ class Artist(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255, blank=True)
     biography = models.TextField(blank=True)
+    favorite_songs = models.ManyToManyField(
+        'tracks.Track',
+        blank=True,
+        related_name='is_favorite_of',
+    )
 
     def __unicode__(self):
         return self.first_name
