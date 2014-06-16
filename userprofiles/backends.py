310649c71fb441c6da21652f4617f4d7a469160a
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2014-06-15 19:02:01 -0500

    authenticate by username or email

--- userprofiles/backends.py
+++ userprofiles/backends.py
@@ -3,9 +3,19 @@ from django.contrib.auth.models import User
 
 class EmailBackend(object):
 
-    def authenticate(self, email=None, password=None):
+    def authenticate(self, **kwargs):
+        username = kwargs.get('username')
+        email = kwargs.get('email')
+        password = kwargs.get('password')
         try:
-            user = User.objects.get(email=email)
+            if email:
+                user = User.objects.get(email=email)
+            else:
+                try:
+                    user = User.objects.get(username=username)
+                except User.DoesNotExist:
+                    user = User.objects.get(email=username)
+            
             if user.check_password(password):
                 return user
         except User.DoesNotExist:
